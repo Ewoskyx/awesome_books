9d35b5ff1ebc0f4ff80ca141cede7c70a0b9cd3e
Ahmed <ahmedreda152@gmail.com>
2021-11-22 14:39:33 +0200

    fix first approach

--- app.js
+++ app.js
@@ -1,34 +1,56 @@
-const bookTitle = document.getElementById("title");
-const bookAuthor = document.getElementById("author");
-const addbtn = document.getElementById("add");
-const dynamicBooksDiv = document.getElementById("books-wrapper");
+const bookTitle = document.getElementById('title');
+const formContainer = document.getElementById('formContainer');
+const bookAuthor = document.getElementById('author');
+const addbtn = document.getElementById('add');
+const dynamicBooksDiv = document.getElementById('books-wrapper');
 
-let books = [];
+let books = JSON.parse(localStorage.getItem('books')) || [];
+console.log(books);
 
-function addBook() {
-  const book = {
-    title: bookTitle.value,
-    author: bookAuthor.value,
-  };
-  books.push(book);
-  localStorage.setItem("Books", JSON.stringify(books));
-}
+const showBooks = () => {
+  books.map((book) => {
+    const bookTest = `
+    <div class='book-div'>
+    <h4><strong>Title:</strong> ${book.title}</h4>
+    <h4>Author: ${book.author}</h4>
+    <button type='button'  class="rmv-btn" data-delete=${book.id}>Remove</button>
+    `;
+    return dynamicBooksDiv.insertAdjacentHTML('beforeend', bookTest);
+  });
+};
 
-addbtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  addBook();
-  showBooks();
-});
+const addBook = (title, author, id) => {
+  books.push({
+    title,
+    author,
+    id,
+  });
 
-function showBooks() {
-  let data = localStorage.getItem("Books");
-  data = JSON.parse(data);
-  const lastAddedBook = data[data.length - 1];
-  dynamicBooksDiv.innerHTML += `
-      <div class='book-div'>
-       <h4><strong>Title:</strong> ${lastAddedBook.title}</h4>
-       <h4>Author: ${lastAddedBook.author}</h4>
-       <button>Remove</button>
-      </div>
-      `;
-}
+  localStorage.setItem('books', JSON.stringify(books));
+
+  return { title, author, id };
+};
+
+formContainer.onsubmit = (e) => {
+  const newBook = addBook(
+    bookTitle.value,
+    bookAuthor.value,
+    Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1),
+  );
+
+};
+
+window.onload = showBooks;
+
+// const removeBook = (bookId) => {
+//   books = books.filter((book) => book.id !== bookId);
+// };
+// const rmvBtn = document.querySelector('.rmv-btn');
+// dynamicBooksDiv.addEventListener('click', (r) => {
+//   if (r.target.classList.contains('rmv-btn')) {
+//     r.target.parentElement.remove();
+//     removeBook(r.target.id);
+//   }
+// });
