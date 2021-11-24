69d6b17917ec33d9984e0e67eea2bc983414b696
Ahmed <ahmedreda152@gmail.com>
2021-11-24 17:38:45 +0200

    add date and time

--- app.js
+++ app.js
@@ -91,12 +91,10 @@ document.querySelector('.books-wrapper').addEventListener('click', (e) => {
   Book.removeBooks(e.target.id);
 });
 
-//styles d4
 const booklistBtn = document.getElementById('booklist');
 const addbookBtn = document.getElementById('addbook');
 const contactinfoBtn = document.getElementById('contactinfo');
 const booklistSection = document.getElementById('booklistsection');
-const header = document.getElementById('header');
 const inputSection = document.querySelector('.input-wrapper');
 const contactinfoSection = document.querySelector('.contactinfo-section');
 const booklistOpen = () => {
@@ -106,15 +104,15 @@ const booklistOpen = () => {
   contactinfoBtn.style.backgroundColor = 'unset';
   addbookBtn.style.backgroundColor = 'unset';
   contactinfoSection.classList.add('dis-none');
-}
+};
 const addbookOpen = () => {
   booklistSection.classList.add('dis-none');
   inputSection.classList.remove('dis-none');
   booklistBtn.style.backgroundColor = 'unset';
-  contactinfoBtn.style.backgroundColor = 'unset'
+  contactinfoBtn.style.backgroundColor = 'unset';
   addbookBtn.style.backgroundColor = 'white';
   contactinfoSection.classList.add('dis-none');
-}
+};
 const contactinfoOpen = () => {
   booklistSection.classList.add('dis-none');
   inputSection.classList.add('dis-none');
@@ -122,7 +120,18 @@ const contactinfoOpen = () => {
   contactinfoBtn.style.backgroundColor = 'white';
   addbookBtn.style.backgroundColor = 'unset';
   contactinfoSection.classList.remove('dis-none');
-}
-booklistBtn.addEventListener('click',booklistOpen);
-addbookBtn.addEventListener('click',addbookOpen);
-contactinfoBtn.addEventListener('click',contactinfoOpen);
+};
+booklistBtn.addEventListener('click', booklistOpen);
+addbookBtn.addEventListener('click', addbookOpen);
+contactinfoBtn.addEventListener('click', contactinfoOpen);
+
+const timeDiv = document.querySelector('.time');
+const span = document.createElement('span');
+
+const currentdate = new Date();
+const datetime = `${currentdate.getDate()}/${
+  currentdate.getMonth() + 1
+}/${currentdate.getFullYear()}  ${currentdate.getHours()}:${currentdate.getMinutes()}:${currentdate.getSeconds()}`;
+
+span.append(datetime);
+timeDiv.appendChild(span);
